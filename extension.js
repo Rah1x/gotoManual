40c572ada600247f911ef5263b6dd7b668353369
Raheel Hasan <rhasan@ct.com.au>
2019-10-18 16:53:04 +1100

    first successful release

--- extension.js
+++ extension.js
@@ -1,24 +1,33 @@
 const vscode = require('vscode');
-const ext_name = "gotomanual";
-const CMD_ID = "extension."+ext_name;
+
+const proj_name = "gotomanual";
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context)
 {
-    //console.log('Congratulations, your extension "gotomanual" is now active!');
+    //console.log(proj_name+' started..')
 
-    const disposable = vscode.commands.registerTextEditorCommand(CMD_ID, openManual);
-
-	//let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+    //let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
     //vscode.window.showInformationMessage('Hello World Hello hello!');
-	//});
+    //});
 
-    context.subscriptions.push(disposable);
+    let config = vscode.workspace.getConfiguration(proj_name+'.url');
+
+    const google_m = vscode.commands.registerCommand('extension.gotoGoogle', function() {
+        openManual(GetSelectedText(), config.get("Google"));
+    }); context.subscriptions.push(google_m);
+
+    const php_m = vscode.commands.registerCommand('extension.gotomanualPHP', function() {
+        openManual(GetSelectedText().replace('_', '-'), config.get("PHP"));
+    }); context.subscriptions.push(php_m);
+
+    const js_m = vscode.commands.registerCommand('extension.gotomanualJS', function() {
+        openManual(GetSelectedText(), config.get("JS"));
+    }); context.subscriptions.push(js_m);
 }
 exports.activate = activate;
-
 
 function deactivate() {}
 //exports.deactivate = deactivate;
@@ -28,21 +37,16 @@ module.exports = {
 	deactivate
 }
 
-// --------------------------------------------
+// ----------------------------------------------------------------------------------------
 
 //Function to launch the Search URL in default browser
-function openManual()
+function openManual(selectedText, settings)
 {
-    var selectedText = GetSelectedText();
     if(!selectedText) return;
 
+    vscode.window.showInformationMessage('GotoManual: finding \''+selectedText+'\'...');
 
-    let searchCfg = vscode.workspace.getConfiguration(ext_name);
-    const queryTemplate = searchCfg.get("QueryTemplate");
-
-    let uriText = encodeURI(selectedText);
-    let query = queryTemplate.replace("%SELECTION%", uriText);
-
+    let query = settings.replace("%SELECTION%", encodeURI(selectedText));
     vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(query));
 }
 
@@ -51,22 +55,17 @@ function openManual()
 function GetSelectedText()
 {
     const documentText = vscode.window.activeTextEditor.document.getText();
-    if (!documentText) {
-      return "";
-    }
+    if (!documentText) return "";
+
     let activeSelection = vscode.window.activeTextEditor.selection;
-    if (activeSelection.isEmpty) {
-      return "";
-    }
-    const selStartoffset = vscode.window.activeTextEditor.document.offsetAt(
-      activeSelection.start
-    );
-    const selEndOffset = vscode.window.activeTextEditor.document.offsetAt(
-      activeSelection.end
-    );
+    if (activeSelection.isEmpty) return "";
+
+    const selStartoffset = vscode.window.activeTextEditor.document.offsetAt(activeSelection.start);
+    const selEndOffset = vscode.window.activeTextEditor.document.offsetAt(activeSelection.end);
 
     let selectedText = documentText.slice(selStartoffset, selEndOffset).trim();
     selectedText = selectedText.replace(/\s\s+/g, " ");
+
     return selectedText;
 }
 
